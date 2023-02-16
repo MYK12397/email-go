01dbddd9185c0ce93513db1724d012fa22a83ceb
MYK12397 <yusufzai.12397@gmail.com>
2023-02-16 17:47:50 +0530

    email service added

---
+++ mail/senderEmail.go
@@ -0,0 +1,63 @@
+package mail
+
+import (
+	"fmt"
+	"net/smtp"
+
+	"github.com/jordan-wright/email"
+)
+
+const (
+	smtpAuthAddress   = "smtp.gmail.com"
+	smtpServerAddress = "smtp.gmail.com:587"
+)
+
+type SenderGmail struct {
+	name              string
+	fromEmailAddress  string
+	fromEmailPassword string
+}
+
+type SenderEmail interface {
+	SendEmail(
+		subject string,
+		body string,
+		to []string,
+		cc []string,
+		bcc []string,
+		attachedFiles []string,
+	) error
+}
+
+func NewSenderGmail(name string, fromEmailAddress string, fromEmailPassword string) SenderEmail {
+
+	return &SenderGmail{
+		name:              name,
+		fromEmailAddress:  fromEmailAddress,
+		fromEmailPassword: fromEmailPassword,
+	}
+}
+
+func (sender *SenderGmail) SendEmail(subject string, body string, to []string, cc []string, bcc []string, attachedFiles []string) error {
+	e := email.NewEmail()
+	e.From = fmt.Sprintf("%s <%s>", sender.name, sender.fromEmailAddress)
+	e.Subject = subject
+	e.HTML = []byte(body) //content
+	e.To = to
+	e.Cc = cc
+	e.Bcc = bcc
+
+	for _, file := range attachedFiles {
+		_, err := e.AttachFile(file)
+		if err != nil {
+			return fmt.Errorf("failed to attach file %s: %w", file, err)
+		}
+
+	}
+
+	smtpAuth := smtp.PlainAuth("", sender.fromEmailAddress, sender.fromEmailPassword, smtpAuthAddress)
+
+	err := e.Send(smtpServerAddress, smtpAuth)
+
+	return err
+}
---
+++ mail/senderEmail_test.go
@@ -0,0 +1,30 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/MYK12397/email-go/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewSenderGmail(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+	<h1>Hello world</h1>
+	<p>This is a test message from <a href="http://github.com/MYK12397">Yahiya Khan</a></p>
+	`
+	to := []string{"yusufzai.12397@gmail.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
